512adf62ba39e9c85e8217029a4ccc63e25d9eb1
Eliana Intriago <eliana.intriago@udla.edu.ec>
2026-05-20 15:12:00 -0500

    funcion calcular PromedioxReferencia

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-float calcularPromedio(float [5]);
+//float calcularPromedio(float [5]);
+float calcularPromedioxRef(float *ptr);
 
 int main (int argc, char *argv[]) {
    float notas[5]={6.5 , 7.1 , 8 , 6 , 5};
-    printf("promedio:%.2f\n",calcularPromedio(notas));
+    printf("promedio:%.2f\n",calcularPromedioxRef(notas));
     return 0;
 }
 
-float calcularPromedio(float notas[5]){
+float calcularPromedioxRef(float *ptr){
       float suma=0;
      for (int i = 0; i < 5; i++) {
-    printf("notas[%d]%.1f\n",i,notas[i]);
-    suma+=notas[i];
+    printf("notas[%d]%.1f\n",i,ptr[i]);
+    suma+=ptr[i];
    }
    float promedio=suma/5.0f;
+   return promedio;
 }
