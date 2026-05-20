d7d1d22a87148b2a0dc11a8a6018f83a883c87ea
Eliana Intriago <eliana.intriago@udla.edu.ec>
2026-05-20 14:48:50 -0500

    segundo commit promedio de notas

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 
 int main (int argc, char *argv[]) {
    float notas[5]={6.5 , 7.1 , 8 , 6 , 5};
-
-   for (it i = 0; i < 5; i++)
+   float suma=0.1f;
+   for (int i = 0; i < 5; i++)
    {
     printf("notas[%d]%.1f\n",i,notas[i]);
+    suma+=notas[i];
    }
-   
+   float promedio=suma/5.0f;
+   printf("promedio:%.2f\n",promedio);
     return 0;
 }
