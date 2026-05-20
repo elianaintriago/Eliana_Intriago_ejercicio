a1f7935fb99924123704dd5c795a6629758ad03c
Eliana Intriago <eliana.intriago@udla.edu.ec>
2026-05-20 15:05:41 -0500

    funcion calcular promedio xValor

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
+float calcularPromedio(float [5]);
+
 int main (int argc, char *argv[]) {
    float notas[5]={6.5 , 7.1 , 8 , 6 , 5};
-   float suma=0.1f;
-   for (int i = 0; i < 5; i++)
-   {
+    printf("promedio:%.2f\n",calcularPromedio(notas));
+    return 0;
+}
+
+float calcularPromedio(float notas[5]){
+      float suma=0;
+     for (int i = 0; i < 5; i++) {
     printf("notas[%d]%.1f\n",i,notas[i]);
     suma+=notas[i];
    }
    float promedio=suma/5.0f;
-   printf("promedio:%.2f\n",promedio);
-    return 0;
 }
